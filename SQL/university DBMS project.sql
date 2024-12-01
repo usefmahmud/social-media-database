231e720e55a032623d3c18c55b1e0f5866178707
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-12-01 12:13:20 +0200

    update SQL with new columns

--- SQL/university DBMS project.sql
+++ SQL/university DBMS project.sql
@@ -21,7 +21,7 @@ CREATE TABLE `interest` (
   `interest` varchar(50)
 );
 
-CREATE TABLE `user_interest` (
+CREATE TABLE `user_interests` (
   `id` int PRIMARY KEY,
   `user_id` int,
   `interest_id` int
@@ -41,26 +41,26 @@ CREATE TABLE `chat` (
   `started_at` timestamp
 );
 
-CREATE TABLE `user_message` (
+CREATE TABLE `user_messages` (
   `message_id` int PRIMARY KEY,
   `chat_id` int,
   `from_id` int,
   `to_id` int,
   `sent_at` timestamp,
-  `message_text` longtext
+  `message_text` varchar(255)
 );
 
 CREATE TABLE `post` (
   `post_id` int PRIMARY KEY,
   `user_id` int,
-  `visiablity` ENUM ('public', 'only_followers', 'community'),
-  `caption` longtext,
+  `visibility` ENUM ('public', 'community'),
+  `caption` mediumtext,
   `posted_at` timestamp
 );
 
 CREATE TABLE `post_photos` (
   `id` int PRIMARY KEY,
-  `url` text,
+  `url` varchar(100),
   `post_id` int
 );
 
@@ -72,20 +72,20 @@ CREATE TABLE `post_videos` (
 );
 
 CREATE TABLE `post_likes` (
-  `id` int PRIMARY KEY,
+  `like_id` int PRIMARY KEY,
   `post_id` int,
   `user_id` int,
   `type` ENUM ('like', 'dislike'),
   `liked_at` timestamp
 );
 
-CREATE TABLE `feedbacks` (
+CREATE TABLE `feedback` (
   `feedback_id` int PRIMARY KEY,
-  `feedback` text,
+  `feedback` varchar(100),
   `created_at` timestamp
 );
 
-CREATE TABLE `post_feedback` (
+CREATE TABLE `post_feedbacks` (
   `id` int PRIMARY KEY,
   `feedback_id` int,
   `dislike_id` int,
@@ -95,21 +95,21 @@ CREATE TABLE `post_feedback` (
 CREATE TABLE `post_repost` (
   `id` int PRIMARY KEY,
   `post_id` int,
-  `user_id` int,
-  `reposted_at` timestamp
+  `user_id` int
 );
 
-CREATE TABLE `post_comment` (
+CREATE TABLE `post_comments` (
   `comment_id` int PRIMARY KEY,
   `post_id` int,
   `user_id` int,
-  `comment_text` longtext,
+  `comment_text` mediumtext,
   `commented_at` timestamp
 );
 
 CREATE TABLE `tags` (
   `tag_id` int PRIMARY KEY,
-  `tag` text
+  `interest_id` int,
+  `tag` varchar(50)
 );
 
 CREATE TABLE `post_tags` (
@@ -122,7 +122,8 @@ CREATE TABLE `community` (
   `community_id` int PRIMARY KEY,
   `title` varchar(100),
   `description` mediumtext,
-  `cover_photo` varchar(100)
+  `cover_photo` varchar(100),
+  `interest_id` int
 );
 
 CREATE TABLE `community_membership` (
@@ -146,67 +147,70 @@ CREATE TABLE `event` (
   `description` mediumtext,
   `location` text,
   `event_date` date,
-  `max_attendees` int
+  `max_attendees` int,
+  `interest_id` int
 );
 
 CREATE TABLE `event_attend` (
   `id` int PRIMARY KEY,
   `user_id` int,
   `event_id` int,
-  `status` ENUM ('interested', 'attended')
-);
-
-CREATE TABLE `event_interests` (
-  `id` int PRIMARY KEY,
-  `event_id` int,
-  `interest_id` int
+  `status` ENUM ('interested', 'attend')
 );
 
 ALTER TABLE `user` ADD FOREIGN KEY (`invited_by`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_interest` (`user_id`);
+ALTER TABLE `user_interests` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `interest` ADD FOREIGN KEY (`interest_id`) REFERENCES `user_interest` (`interest_id`);
+ALTER TABLE `user_interests` ADD FOREIGN KEY (`interest_id`) REFERENCES `interest` (`interest_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_follow` (`follower_id`);
+ALTER TABLE `user_follow` ADD FOREIGN KEY (`follower_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_follow` (`following_id`);
+ALTER TABLE `user_follow` ADD FOREIGN KEY (`following_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_message` (`from_id`);
+ALTER TABLE `chat` ADD FOREIGN KEY (`user_1`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `user_message` (`to_id`);
+ALTER TABLE `chat` ADD FOREIGN KEY (`user_2`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `chat` ADD FOREIGN KEY (`chat_id`) REFERENCES `user_message` (`chat_id`);
+ALTER TABLE `user_messages` ADD FOREIGN KEY (`from_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `post` (`user_id`);
+ALTER TABLE `user_messages` ADD FOREIGN KEY (`to_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `user_messages` ADD FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`);
+
+ALTER TABLE `post` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 ALTER TABLE `post_photos` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
 ALTER TABLE `post_videos` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `post_likes` (`user_id`);
+ALTER TABLE `post_likes` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `post` ADD FOREIGN KEY (`post_id`) REFERENCES `post_likes` (`post_id`);
+ALTER TABLE `post_likes` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
-ALTER TABLE `post_feedback` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
+ALTER TABLE `post_feedbacks` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
-ALTER TABLE `post_feedback` ADD FOREIGN KEY (`dislike_id`) REFERENCES `post_likes` (`user_id`);
+ALTER TABLE `post_feedbacks` ADD FOREIGN KEY (`dislike_id`) REFERENCES `post_likes` (`like_id`);
 
-ALTER TABLE `post_feedback` ADD FOREIGN KEY (`feedback_id`) REFERENCES `feedbacks` (`feedback_id`);
+ALTER TABLE `post_feedbacks` ADD FOREIGN KEY (`feedback_id`) REFERENCES `feedback` (`feedback_id`);
 
 ALTER TABLE `post_repost` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 ALTER TABLE `post_repost` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
-ALTER TABLE `post` ADD FOREIGN KEY (`post_id`) REFERENCES `post_comment` (`post_id`);
+ALTER TABLE `post_comments` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `post_comment` (`user_id`);
+ALTER TABLE `post_comments` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+ALTER TABLE `tags` ADD FOREIGN KEY (`interest_id`) REFERENCES `interest` (`interest_id`);
 
 ALTER TABLE `post_tags` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
 ALTER TABLE `post_tags` ADD FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `community_membership` (`user_id`);
+ALTER TABLE `community` ADD FOREIGN KEY (`interest_id`) REFERENCES `interest` (`interest_id`);
+
+ALTER TABLE `community_membership` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 ALTER TABLE `community_membership` ADD FOREIGN KEY (`community_id`) REFERENCES `community` (`community_id`);
 
@@ -214,12 +218,10 @@ ALTER TABLE `community_posts` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`po
 
 ALTER TABLE `community_posts` ADD FOREIGN KEY (`community_id`) REFERENCES `community` (`community_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `event` (`creator_id`);
+ALTER TABLE `event` ADD FOREIGN KEY (`creator_id`) REFERENCES `user` (`user_id`);
 
-ALTER TABLE `event` ADD FOREIGN KEY (`event_id`) REFERENCES `event_attend` (`event_id`);
+ALTER TABLE `event` ADD FOREIGN KEY (`interest_id`) REFERENCES `interest` (`interest_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`user_id`) REFERENCES `event_attend` (`user_id`);
+ALTER TABLE `event_attend` ADD FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`);
 
-ALTER TABLE `event` ADD FOREIGN KEY (`event_id`) REFERENCES `event_interests` (`event_id`);
-
-ALTER TABLE `interest` ADD FOREIGN KEY (`interest_id`) REFERENCES `event_interests` (`interest_id`);
+ALTER TABLE `event_attend` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
