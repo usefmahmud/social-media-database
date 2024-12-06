a282ad66ed99e58e2eb8ff9f78c5e2a3c1226d34
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-12-06 06:56:46 +0200

    restructre the queries

--- queries.sql
+++ queries.sql
@@ -1,26 +1,119 @@
--- most active users in texting
+/*
+	Name: Yousef Mahmoud Abdulaziz
+    Program: CS & Math
+    Project: Social Media Platform
+    Team: MinDB
+ */
+ 
+-- 1 - each user full name with his eduction, country, and city
 select
-    concat(user.first_name, ' ', user.last_name) as full_name,
-    count(user_messages.from_id) as "from",
-    count(user_messages.to_id) as "to"
+    concat(first_name, ' ', last_name) as full_name,
+    education,
+    country,
+    city
 from user
-	left join user_messages
-		on user.user_id = user_messages.from_id 
-			or user.user_id = user_messages.to_id
-group by user.user_id
-order by count(user_messages.from_id) desc, count(user_messages.to_id) desc;
+order by full_name;
 
--- top 5 interests
+-- 2 - top 10 users active in posting
 select
-    interest,
-    count(user_id)
-from interest
-         left join user_interests on interest.interest_id = user_interests.interest_id
-group by interest
-order by count(user_id) desc
+    user.user_id,
+    concat(user.first_name, ' ', user.last_name) as 'Full Name',
+    count(post.post_id) as "Number of Posts"
+from user
+	left join post on post.user_id = user.user_id
+group by user.user_id
+order by count(post.post_id) desc;
+
+
+-- 3 - posts that contain word "help"
+select
+    post_id,
+    caption,
+    posted_at
+from post
+where caption like '%help%';
+
+
+
+-- 4 - most liked posts
+select
+    post_id,
+    caption,
+    (select count(*) from post_likes where post_id = post.post_id) as like_count
+from post
+order by like_count desc
 limit 5;
 
--- each user with his number of invitations
+
+
+-- 5 - average number of likes on posts
+select
+    avg(likes_count) as "likes average"
+from (
+         select
+             count(*) as likes_count
+         from post_likes
+         group by post_id
+     ) as s;
+     
+     
+-- 6 - list of users who joined in the last 3 months
+select
+    user.user_id,
+    concat(user.first_name, ' ', user.last_name) as "Full Name",
+    user.created_at as "Joined Date"
+from user
+where created_at >= now() - interval 3 month;
+
+
+
+-- 7 - users who didn't post in the last 2 years
+select
+    user.user_id,
+    user.first_name
+from user
+where user.user_id not in (
+    select
+        post.user_id
+    from post
+    where post.posted_at >= now() - interval 2 year
+);
+
+
+
+-- 8 - what is the best hour for posting?
+select
+    hour(posted_at) as post_hour,
+    count(*) as post_count
+from post
+group by post_hour
+order by post_count desc;
+
+
+
+-- 9 - reposts number of the posts that have word "environmental"
+select
+    count(post_reposts.user_id) as "Number of Reposts"
+from post
+	left join post_reposts 
+		on post_reposts.post_id = post.post_id
+where post.caption like '%environmental%';
+
+
+
+-- 10 - list countries whith their total interactions number
+select
+    user.country,
+    (select count(*) from post_likes where user_id = user.user_id) +
+    (select count(*) from post_comments where user_id = user.user_id) +
+    (select count(*) from post_reposts where user_id = user.user_id) as total_interactions
+from user
+group by user.country
+order by total_interactions desc;
+
+
+
+-- 11 - each user with his number of invitations
 select
     concat(a.first_name, ' ', a.last_name) as "Full Name",
     count(b.invited_by) as "Number of Intivations"
@@ -30,7 +123,8 @@ from user a
 group by a.user_id
 order by count(b.invited_by) desc;
 
--- each user with number of attending and interested events
+
+-- 12 - each user with number of attending and interested events
 select
     user.first_name,
     coalesce(sum(case when event_attends.status = 'attend' then 1 else 0 end)) as "Attending",
@@ -40,17 +134,9 @@ from user
 		on user.user_id = event_attends.user_id
 group by user.user_id;
 
--- events which have no attendees
-select
-    event.title
-from event
-	left join event_attends
-		on event.event_id = event_attends.event_id
-			and event_attends.status = 'attend'
-group by event.event_id
-having count(event_attends.event_id) = 0;
 
--- get range of birth dates
+
+-- 13 - get range of birth dates
 select
     (
         select
@@ -64,7 +150,7 @@ select
     ) as "End of Range";
 
 
--- how many users born in each 5 years
+-- 14 - how many users born in each 5 years
 select
     case
         when year(birth_date) between 1990 and 1995 then '1990 - 1995'
@@ -76,7 +162,23 @@ select
 from user
 group by birth_date_range;
 
--- get all people who "Bobbie Perrigo" talked with
+
+
+-- 15 - most active users in texting
+select
+    concat(user.first_name, ' ', user.last_name) as full_name,
+    count(user_messages.from_id) as "from",
+    count(user_messages.to_id) as "to"
+from user
+	left join user_messages
+		on user.user_id = user_messages.from_id 
+			or user.user_id = user_messages.to_id
+group by user.user_id
+order by count(user_messages.from_id) desc, count(user_messages.to_id) desc;
+
+
+
+-- 16 - get all people who "Bobbie Perrigo" talked with
 select
     concat(other.first_name, ' ', other.last_name) as "Full Name"
 from user person
@@ -88,90 +190,9 @@ from user person
 		and other.user_id <> person.user_id
 where person.first_name = 'Bobbie'
   and person.last_name = 'Perrigo';
+  
 
-
--- list of users who joined in the last 3 months
-select
-    user.user_id,
-    concat(user.first_name, ' ', user.last_name) as "Full Name",
-    user.created_at as "Joined Date"
-from user
-where created_at >= now() - interval 3 month;
-
-
--- user who didn't post in the last 2 years
-select
-    user.user_id,
-    user.first_name
-from user
-where user.user_id not in (
-    select
-        post.user_id
-    from post
-    where post.posted_at >= now() - interval 2 year
-);
-
-
--- most liked posts
-select
-    post_id,
-    caption,
-    (select count(*) from post_likes where post_id = post.post_id) as like_count
-from post
-order by like_count desc
-limit 5;
-
-
--- posts that contain word "help"
-select
-    post_id,
-    caption,
-    posted_at
-from post
-where caption like '%help%';
-
--- average number of likes on posts
-select
-    avg(likes_count) as "likes average"
-from (
-         select
-             count(*) as likes_count
-         from post_likes
-         group by post_id
-     ) as s;
-
-
--- what is the best hour for posting?
-select
-    hour(posted_at) as post_hour,
-    count(*) as post_count
-from post
-group by post_hour
-order by post_count desc;
-
-
--- list countries whith their total interactions number
-select
-    user.country,
-    (select count(*) from post_likes where user_id = user.user_id) +
-    (select count(*) from post_comments where user_id = user.user_id) +
-    (select count(*) from post_reposts where user_id = user.user_id) as total_interactions
-from user
-group by user.country
-order by total_interactions desc;
-
-
--- events will happen in next month
-select
-    event_id,
-    title,
-    event_date,
-    location
-from event
-where event_date >= now()
-  and event_date <= now() + interval 1 month;
-
--- events created by "Ronny Bew"
+-- 17 - events created by "Ronny Bew"
 select
     event_id,
     title,
@@ -184,14 +205,56 @@ where creator_id = (
     where user.first_name = 'Ronny'
       and user.last_name = 'Bew'
 );
+  
 
 
--- average age of users
+-- 18 events which have no attendees
+select
+    event.title
+from event
+	left join event_attends
+		on event.event_id = event_attends.event_id
+			and event_attends.status = 'attend'
+group by event.event_id
+having count(event_attends.event_id) = 0;
+
+
+
+-- 19 - events that hit maximum attendees
+select
+    event.event_id,
+    event.title,
+    event.max_attendees,
+    count(event_attends.user_id) as "actual attendees"
+from event
+	left join event_attends
+		on event.event_id = event_attends.event_id
+			and event_attends.status = 'attend'
+group by event.event_id, event.max_attendees
+having count(event_attends.user_id) > event.max_attendees;
+
+
+
+-- 20 events will happen in next month
+select
+    event_id,
+    title,
+    event_date,
+    location
+from event
+where event_date >= now()
+  and event_date <= now() + interval 1 month;
+
+
+
+-- 21 - average age of users
 select
     round(avg(datediff(now(), birth_date) / 365))
 from user;
 
--- most frequent age
+
+
+-- 22 - most frequent age
 select
     round(datediff(now(), birth_date) / 365) as age,
     count(user_id) as "Number of Users"
@@ -199,7 +262,30 @@ from user
 group by age
 order by count(user_id);
 
--- top 10 ternding topics
+
+
+-- number of given feedbacks
+select
+    feedback,
+    count(like_id) as "Given Feedbacks"
+from feedback
+group by feedback
+order by count(like_id) desc;
+
+
+-- 23 - top 5 interests for users
+select
+    interest,
+    count(user_id)
+from interest
+         left join user_interests on interest.interest_id = user_interests.interest_id
+group by interest
+order by count(user_id) desc
+limit 5;
+
+
+
+-- 24 - top 10 ternding topics
 select
     tags.tag,
     interest.interest,
@@ -214,7 +300,129 @@ order by count(post_tags.post_id) desc
 limit 10;
 
 
--- each user weekly growth
+
+
+-- 25 - show all users with their followers and following number
+select
+    user.user_id,
+    user.first_name,
+    coalesce(followers.follower_count, 0) as followers_count,
+    coalesce(following.following_count, 0) as following_count
+from user
+    left join (
+        select
+            following_id as user_id,
+            count(follower_id) as follower_count
+        from user_follow
+        group by following_id
+    ) as followers  on user.user_id = followers.user_id
+    left join (
+        select
+            follower_id as user_id,
+            count(following_id) as following_count
+        from user_follow
+        group by follower_id
+    ) as following  on user.user_id = following.user_id
+order by followers_count desc, following_count desc;
+
+
+
+-- 26 - each community, its interest, and number of admins and members
+select
+    community.title,
+    interest.interest,
+    sum(case when community_membership.type = 'admin' then 1 else 0 end) as admins,
+    sum(case when community_membership.type = 'member' then 1 else 0 end) as members
+from community
+	left join interest
+		on interest.interest_id = community.interest_id
+	left join community_membership
+		on community.community_id = community_membership.community_id
+group by community.community_id
+order by members desc, admins desc;
+
+
+
+-- 27 - showing each post details with its engagement
+select
+    user.first_name,
+    post.caption,
+    (case when post.visibility = 'public' then 'public' else community.title end) as visibility,
+    (select count(*) from post_likes where post_likes.post_id = post.post_id) as like_count,
+    (select count(*) from post_comments where post_comments.post_id = post.post_id) as comment_count,
+    (select count(*) from post_reposts where post_reposts.post_id = post.post_id) as repost_count
+from post
+	left join user 
+		on post.user_id = user.user_id
+	left join community_posts 
+		on post.post_id = community_posts.post_id
+	left join community 
+		on community.community_id = community_posts.community_id
+order by user.first_name,like_count desc, comment_count desc, repost_count desc;
+
+
+
+-- 28 tag combinations posts count
+select
+    t1.tag as tag1,
+    t2.tag as tag2,
+    count(distinct pt1.post_id) as post_count
+from post_tags pt1
+	left join post_tags pt2
+		on pt1.post_id = pt2.post_id
+			and pt1.tag_id < pt2.tag_id
+	join tags t1 
+		on pt1.tag_id = t1.tag_id
+	join tags t2 
+		on pt2.tag_id = t2.tag_id
+group by t1.tag, t2.tag
+order by post_count desc;
+
+
+-- 29 - Is there any posts that talks about topics differ from community main topic?
+select
+    community.community_id,
+    post.post_id,
+    post.caption,
+    tags.tag as post_tag
+from post
+	left join post_tags
+		on post.post_id = post_tags.post_id
+	left join tags
+		on post_tags.tag_id = tags.tag_id
+	left join community_posts
+		on post.post_id = community_posts.post_id
+	left join community
+		on community.community_id = community_posts.community_id
+where tags.interest_id != community.interest_id
+group by community.community_id;
+
+
+-- 30 - posts with their top negative feedback
+select
+    post.post_id,
+    post.caption,
+    feedback.feedback as "Most Feedback"
+from post
+	left join post_likes 
+		on post_likes.post_id = post.post_id
+	left join feedback 
+		on feedback.like_id = post_likes.like_id
+where feedback.feedback = (
+    select
+        feedback.feedback
+    from feedback
+		join post_likes 
+			on feedback.like_id = post_likes.like_id
+    where post_likes.post_id = post.post_id
+    group by feedback.feedback
+    order by count(*) desc
+    limit 1
+)
+group by post.post_id;
+
+
+-- out - each user weekly growth
 with weekly_activity as (
     select
         user_id,
@@ -248,187 +456,9 @@ where growth > 0
 order by growth desc;
 
 
--- number of posts per user
-select
-    user.user_id,
-    concat(user.first_name, ' ', user.last_name) as 'Full Name',
-    count(post.post_id)
-from user
-	left join post on post.user_id = user.user_id
-group by user.user_id
-order by count(post.post_id);
-
-
---
-select user_id, count(*) as post_count
-from post
-group by user_id
-order by post_count desc
-limit 10;
-
-
--- enagement types count per year
-
--- showing each post details with its engagement
-select
-    user.first_name,
-    post.caption,
-    (case when post.visibility = 'public' then 'public' else community.title end) as visibility,
-    (select count(*) from post_likes where post_likes.post_id = post.post_id) as like_count,
-    (select count(*) from post_comments where post_comments.post_id = post.post_id) as comment_count,
-    (select count(*) from post_reposts where post_reposts.post_id = post.post_id) as repost_count
-from post
-	left join user 
-		on post.user_id = user.user_id
-	left join community_posts 
-		on post.post_id = community_posts.post_id
-	left join community 
-		on community.community_id = community_posts.community_id
-order by user.first_name,like_count desc, comment_count desc, repost_count desc;
-
-
--- show users with their engagement score
-
--- reposts number of the posts that have word "environmental"
-select
-    count(post_reposts.user_id) as "Number of Reposts"
-from post
-	left join post_reposts 
-		on post_reposts.post_id = post.post_id
-where post.caption like '%environmental%';
-
--- top voice people in each interest
-
-
--- each community, its interest, and number of admins and members
-select
-    community.title,
-    interest.interest,
-    sum(case when community_membership.type = 'admin' then 1 else 0 end) as admins,
-    sum(case when community_membership.type = 'member' then 1 else 0 end) as members
-from community
-	left join interest
-		on interest.interest_id = community.interest_id
-	left join community_membership
-		on community.community_id = community_membership.community_id
-group by community.community_id
-order by members desc, admins desc;
-
--- events that hit maximum attendees
-select
-    event.event_id,
-    event.title,
-    event.max_attendees,
-    count(event_attends.user_id) as "actual attendees"
-from event
-	left join event_attends
-		on event.event_id = event_attends.event_id
-			and event_attends.status = 'attend'
-group by event.event_id, event.max_attendees
-having count(event_attends.user_id) > event.max_attendees;
-
-
--- posts with their top negative feedback
-select
-    post.post_id,
-    post.caption,
-    feedback.feedback as "Most Feedback"
-from post
-	left join post_likes 
-		on post_likes.post_id = post.post_id
-	left join feedback 
-		on feedback.like_id = post_likes.like_id
-where feedback.feedback = (
-    select
-        feedback.feedback
-    from feedback
-		join post_likes 
-			on feedback.like_id = post_likes.like_id
-    where post_likes.post_id = post.post_id
-    group by feedback.feedback
-    order by count(*) desc
-    limit 1
-)
-group by post.post_id;
-
-
--- show all users with their followers and following number
-select
-    user.user_id,
-    user.first_name,
-    coalesce(followers.follower_count, 0) as followers_count,
-    coalesce(following.following_count, 0) as following_count
-from user
-    left join (
-        select
-            following_id as user_id,
-            count(follower_id) as follower_count
-        from user_follow
-        group by following_id
-    ) as followers  on user.user_id = followers.user_id
-    left join (
-        select
-            follower_id as user_id,
-            count(following_id) as following_count
-        from user_follow
-        group by follower_id
-    ) as following  on user.user_id = following.user_id
-order by followers_count desc, following_count desc;
-
--- each user full name with his eduction, country, and city
-select
-    concat(first_name, ' ', last_name) as full_name,
-    education,
-    country,
-    city
-from user
-order by full_name;
-
--- number of given feedbacks
-select
-    feedback,
-    count(like_id) as "Given Feedbacks"
-from feedback
-group by feedback
-order by count(like_id) desc;
-
 -- users who are mutual followers
 select count(distinct f1.follower_id, f1.following_id)
 from user_follow f1
 	join user_follow f2 
 		on f1.follower_id = f2.following_id 
 			and f1.following_id = f2.follower_id;
-
--- tag combinations posts count
-select
-    t1.tag as tag1,
-    t2.tag as tag2,
-    count(distinct pt1.post_id) as post_count
-from post_tags pt1
-	left join post_tags pt2
-		on pt1.post_id = pt2.post_id
-			and pt1.tag_id < pt2.tag_id
-	join tags t1 
-		on pt1.tag_id = t1.tag_id
-	join tags t2 
-		on pt2.tag_id = t2.tag_id
-group by t1.tag, t2.tag
-order by post_count desc;
-
--- Is there any posts that talks about topics differ from community main topic?
-select
-    community.community_id,
-    post.post_id,
-    post.caption,
-    tags.tag as post_tag
-from post
-	left join post_tags
-		on post.post_id = post_tags.post_id
-	left join tags
-		on post_tags.tag_id = tags.tag_id
-	left join community_posts
-		on post.post_id = community_posts.post_id
-	left join community
-		on community.community_id = community_posts.community_id
-where tags.interest_id != community.interest_id
-group by community.community_id;
